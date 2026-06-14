62c78c62d533c63848f2b98420f3f0e5ccfc22e1
Feras Shtiewi <ferasshtiewi@Ferass-MacBook-Air.local>
2026-06-14 10:54:54 +0300

    Update website11

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -2,47 +2,45 @@
 
 import Image from "next/image"
 import Link from "next/link"
+import { Button } from "@/Components/ui/button"
 
 export default function Navbar() {
   return (
-    <nav className="sticky top-0 z-50 bg-white shadow-sm">
-      <div className="relative max-w-7xl mx-auto h-24 px-6">
+    <nav className="sticky top-0 z-50 flex items-center justify-between px-6 md:px-12 py-2 md:py-3 backdrop-blur-md bg-white/30 border-b border-white/20 shadow-sm">
 
-        {/* Logo */}
-        <div className="absolute left-6 top-1/2 -translate-y-1/2">
-          <Link href="/">
-            <Image
-              src="/koza.png"
-              alt="Koza Construction Logo"
-              width={140}
-              height={70}
-              priority
-            />
-          </Link>
-        </div>
 
-        {/* Centered Menu */}
-        <div className="flex h-full items-center justify-center">
-          <div className="flex gap-10 text-gray-800 font-medium">
-            <Link href="#about" className="hover:text-orange-500 transition">
-              About
-            </Link>
+      <Link href="/" className="flex items-center gap-2">
+        <Image
+          src="/koza.png"
+          alt="Koza Construction Logo"
+          width={120}
+          height={60}
+          priority
+        />
+      </Link>
 
-            <Link href="#services" className="hover:text-orange-500 transition">
-              Services
-            </Link>
-
-            <Link href="#projects" className="hover:text-orange-500 transition">
-              Projects
-            </Link>
-
-            <Link href="#contact" className="hover:text-orange-500 transition">
-              Contact
-            </Link>
-          </div>
-        </div>
-
+     
+      <div className="flex flex-wrap gap-4 md:gap-6 text-gray-800 font-medium">
+        <Link href="#about" className="hover:text-orange-500 transition">
+          About
+        </Link>
+        <Link href="#services" className="hover:text-orange-500 transition">
+          Services
+        </Link>
+        <Link href="#projects" className="hover:text-orange-500 transition">
+          Projects
+        </Link>
+        <Link href="#contact" className="hover:text-orange-500 transition">
+          Contact
+        </Link>
       </div>
+
+      <Button className="transition-shadow duration-300 hover:shadow-lg hover:shadow-orange-400/50">
+      <a href="mailto:mohd.youssef@optimizedholding.com.qasubject=Get Quote">
+    Get Quote
+  </a>
+      </Button>
+
     </nav>
   )
 }
